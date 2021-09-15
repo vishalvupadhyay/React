ae2c55f21f7e1ff68d199f36d9dd1c578f9e290b
Vishal Upadhyay <vishalu@nvidia.com>
2021-09-15 12:54:46 +0530

    new images

--- 1_jsx/README.md
+++ 1_jsx/README.md
@@ -2,3 +2,9 @@
 
 ## JSX Overview
 ![jsx](../images/3_JSX.png)
+
+## HTML VS JSX
+![html_vs_jsx](../images/4_HTML_VS_JSX.png)
+
+
+![html_vs_jsx](../images/4.1_HTML_VS_JSX.png)
--- 1_jsx/src/index.js
+++ 1_jsx/src/index.js
@@ -2,7 +2,15 @@ import react from "react";
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hello</div>
+  const buttonText = "Submit"
+  return (
+    <div>
+      <label className="label" htmlFor="name">Enter Name: </label>
+      <input id="name" />
+      {/* Using variable as button name */}
+      <button type="submit" style={{ backgroundColor:'blue', color:'white' }}> {buttonText} </button>
+    </div> 
+  )
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
